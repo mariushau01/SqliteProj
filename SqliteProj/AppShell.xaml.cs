72682c988d077e800df40c312e98eac34deefe67
Marius Hau <marius.hau@icloud.com>
2024-04-25 08:20:34 +0200

    Projektdateien hinzufügen.

---
+++ SqliteProj/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SqliteProj
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SqliteProj/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace SqliteProj
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+      
+    }
+
+}
